e62e5bdd5619fb8d460557f40161ee7431ba5f49
prashantrajput11 <prashanttanwar.work@gmail.com>
2025-01-06 12:39:02 +0530

    fixes for clustering

--- src/screens/BeatDetailScreen.js
+++ src/screens/BeatDetailScreen.js
@@ -1,65 +1,54 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React, {useRef} from 'react';
-import MapView from 'react-native-map-clustering';
+import React from 'react';
+import {ClusterMap} from 'react-native-cluster-map';
 import {Marker} from 'react-native-maps';
 import {useRoute} from '@react-navigation/native';
-import {ClusterMap} from 'react-native-cluster-map';
 
+// initail region for the map rendering
 const INITIAL_REGION = {
-  latitude: 52.5,
-  longitude: 19.2,
-  latitudeDelta: 8.5,
-  longitudeDelta: 8.5,
+  latitude: 28.4595,
+  longitude: 77.0266,
+  latitudeDelta: 1.5,
+  longitudeDelta: 1.5,
 };
-const BeatDetailScreen = () => {
-  const mapRef = useRef();
-  const {beatData} = useRoute()?.params;
 
+const BeatDetailScreen = () => {
+  const {beatData} = useRoute()?.params;
   const {stores} = beatData;
 
-  const animateToRegion = () => {
-    let region = {
-      latitude: 42.5,
-      longitude: 15.2,
-      latitudeDelta: 7.5,
-      longitudeDelta: 7.5,
-    };
-
-    mapRef.current.animateToRegion(region, 2000);
+  const renderCustomClusterMarker = cluster => {
+    const {pointCount} = cluster;
+    return <MyCluster count={pointCount} />;
   };
+
+  const MyCluster = ({count}) => (
+    <View style={styles.cluster}>
+      <Text style={styles.clusterText}>{count}</Text>
+    </View>
+  );
 
   return (
     <View style={{flex: 1}}>
       <Text style={styles.header}>BeatDetailScreen</Text>
-      {/* <MapView
-        provider="google"
-        ref={mapRef}
-        initialRegion={INITIAL_REGION}
-        style={{flex: 1}}>
-        {stores.map(store => (
-          <Marker
-            key={store.id}
-            coordinate={{
-              latitude: 52.5,
-              longitude: 19.2,
-            }}
-            title={store.name}
-            description={store.address}
-          />
-        ))}
-      </MapView> */}
       <ClusterMap
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}>
-        <Marker coordinate={{latitude: 37.78725, longitude: -122.434}} />
-        <Marker coordinate={{latitude: 37.789, longitude: -122.431}} />
-        <Marker coordinate={{latitude: 37.78825, longitude: -122.4324}} />
+        renderClusterMarker={renderCustomClusterMarker}
+        provider="google"
+        region={INITIAL_REGION}
+        style={{flex: 1}}>
+        {stores.map(store =>
+          store.latitude && store.longitude ? (
+            <Marker
+              key={store.id}
+              coordinate={{
+                latitude: store.latitude,
+                longitude: store.longitude,
+              }}
+              title={store.name}
+              description={store.address}
+            />
+          ) : null,
+        )}
       </ClusterMap>
-      ;
     </View>
   );
 };
@@ -71,5 +60,17 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 18,
     marginVertical: 10,
+  },
+  cluster: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: 'orange',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  clusterText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
